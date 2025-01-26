327951759641fe4ad2b6d7e62bdeb46413e1fd83
harmy-fgc <strzalla.shawn@web.de>
2025-01-26 17:06:08 +0100

    Angle matrix, all files

---
+++ angle matrix/keymaps/harmy/keymap.c
@@ -0,0 +1,21 @@
+// Copyright (c) 2022 Marco Pelegrini <marco@pelegrini.ca>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┐
+     * │ 7 │ 8 │ 9 │
+     * ├───┼───┼───┤
+     * │ 4 │ 5 │ 6 │
+     * ├───┼───┼───┤
+     * │ 1 │ 2 │ 3 │
+     * └───┴───┴───┘
+     */
+    [0] = LAYOUT(
+        KC_A,   KC_B,   KC_C,   KC_D,   KC_E,
+        KC_F,   KC_G,   KC_H,   KC_I,   KC_J,
+        KC_K,   KC_L,   KC_M,   KC_N,   KC_O
+    )
+};
