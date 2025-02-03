141f2c6201c046fa6d00d7e1dc5fa62aee5759a4
harmy-fgc <strzalla.shawn@web.de>
2025-02-03 21:23:52 +0100

    First_keeb Data

---
+++ First_keeb/4x6/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define EE_HANDS // Store which side I am in EEPROM
+
+/* Keyboard matrix assignments */
+#define MATRIX_ROW_PINS { GP26, GP27, GP28, GP29, }
+#define MATRIX_COL_PINS { GP8, GP7, GP6, GP3, GP2, }
+
