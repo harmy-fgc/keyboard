4599f1c5a2ea60656488e4b10b49a4acecb40fa3
harmy-fgc <strzalla.shawn@web.de>
2025-02-05 14:55:13 +0100

    organization

---
+++ First_keeb/4x6/rules.mk
@@ -0,0 +1,12 @@
+# Build Options
+#   change yes to no to disable
+
+AUDIO_SUPPORTED = no
+RGB_MATRIX_SUPPORTED = no
+RGBLIGHT_SUPPORTED = no
+BOOTMAGIC_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+SERIAL_DRIVER = vendor
+SOFT_SERIAL_ENABLE = no
+CONSOLE_ENABLE = yes 
+ENCODER_ENABLE = no
