a93ff04280ea8832232b1e035bce90b99d6d9d04
harmy-fgc <strzalla.shawn@web.de>
2025-02-06 19:17:26 +0100

    update

--- First_keeb/keymaps/harmy2/keymap.c
+++ First_keeb/keymaps/harmy2/keymap.c
@@ -23,13 +23,13 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
 // ╭──────────────────────────────────────────────────────╮                        ╭──────────────────────────────────────────────────────╮
-                              KC_ESC,  KC_1,  KC_R,    KC_T,                                                  //KC_Y,    KC_U,   KC_8,    KC_9,   //8
+                      KC_ESC,  KC_1,  KC_R,    KC_T, KC_TAB,                                                  //KC_Y,    KC_U,   KC_8,    KC_9,   //8
   // ├──────────────────────────────────────────────────────┤                        ├──────────────────────────────────────────────────────┤
-        KC_TAB,    KC_Q,    KC_W,    KC_E,   KC_F,    KC_G,                              // KC_H,    KC_J,  KC_I,     KC_O,    KC_P,   KC_BSLS, //9-20
+        KC_KP_1,    KC_Q,    KC_W,    KC_E,   KC_F,    KC_G,                              // KC_H,    KC_J,  KC_I,     KC_O,    KC_P,   KC_BSLS, //9-20
   // ├──────────────────────────────────────────────────────┤                        ├──────────────────────────────────────────────────────┤
        KC_LSFT,    KC_A,    KC_S,    KC_D,  KC_V,    KC_B,                              //  KC_N,   KC_M,   KC_K,     KC_L,    KC_SCLN, KC_QUOT, // 21-32
   // ├──────────────────────────────────────────────────────┤                        ├──────────────────────────────────────────────────────┤
-       KC_LCTL,    KC_Z,    KC_X,    KC_C, KC_KP_1, KC_BSPC,  KC_SPC,          // KC_ENT,  KC_DEL, KC_LGUI, KC_COMM,  KC_DOT, KC_SLSH, KC_LALT //32 - 46
+       KC_LCTL,    KC_Z,    KC_X,    KC_C,  KC_BSPC,  KC_SPC,          // KC_ENT,  KC_DEL, KC_LGUI, KC_COMM,  KC_DOT, KC_SLSH, KC_LALT //32 - 46
   // ╰──────────────────────────────────────────────────────┤                        ├──────────────────────────────────────────────────────╯
 
 
